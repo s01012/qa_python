bd2fa5efd4363612d890fb84b64ab9d5b2f7a7e5
Хакимов Эльдар Раисович <hackimov.el@yandex.ru>
2024-06-30 20:22:50 +0300

    Составил 4 теста, решил схранить изменения

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -8,17 +11,25 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.fixture(autouse='True')
+    def books_collector_init(self):
+        self.collector = BooksCollector()
+        self.collector.add_new_book('Гордость и предубеждение и зомби')
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        self.collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        self.collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Фантастика')
+        return self.collector
+    def test_add_new_book_add_one_books(self):
+        self.collector.add_new_book('Му-му')
+        assert len(self.collector.get_books_genre()) == 3
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_set_book_genre_set_new_genre(self):
+        self.collector.set_book_genre('Гордость и предубеждение и зомби', 'Мультфильмы')
+        assert self.collector.get_book_genre('Гордость и предубеждение и зомби') == 'Мультфильмы'
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_book_genre_call_get_book_genre(self):
+        assert self.collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_books_with_specific_genre(self):
+        specific_genre = self.collector.get_books_with_specific_genre('Ужасы')
+        assert specific_genre[0] == 'Гордость и предубеждение и зомби'
